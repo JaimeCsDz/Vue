84c384ae2f89a8e2a1f6eb634cd031db515987d4
Jaime <jaimecastaneda133@gmail.com>
2024-04-14 20:37:16 -0500

    prueba

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: 'https://JaimeCsDz.github.io/Vue/'
+  base: 'https://JaimeCsDz.github.io/'
 })
